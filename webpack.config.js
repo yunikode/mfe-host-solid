5ee81cce86a9b226e78199f7d79cb1c74d1a68f5
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2021-11-14 11:23:03 +0100

    update webpack config for modes

--- webpack.config.js
+++ webpack.config.js
@@ -2,61 +2,68 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const deps = require("./package.json").dependencies;
-module.exports = {
-  output: {
-    publicPath: "http://localhost:8080/",
-  },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+module.exports = (options) => {
+  return {
+    output: {
+      publicPath: options.WEBPACK_BUILD
+        ? "https://zee-german.github.io/mfe-host-solid/"
+        : "http://localhost:8080/",
+    },
 
-  devServer: {
-    port: 8080,
-    historyApiFallback: true,
-  },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+    devServer: {
+      port: 8080,
+      historyApiFallback: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "host",
+        filename: "remoteEntry.js",
+        remotes: {
+          remote: options.WEBPACK_BUILD
+            ? "remote@https://zee-german.github.io/mfe-remote-solid/remoteEntry.js"
+            : "remote@http://localhost:3000/remoteEntry.js",
+        },
+        exposes: {},
+        shared: {
+          ...deps,
+          "solid-js": {
+            singleton: true,
+            requiredVersion: deps["solid-js"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "host",
-      filename: "remoteEntry.js",
-      remotes: {
-        remote: "remote@http://localhost:3000/remoteEntry.js"
-      },
-      exposes: {},
-      shared: {
-        ...deps,
-        "solid-js": {
-          singleton: true,
-          requiredVersion: deps["solid-js"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
+  };
 };
